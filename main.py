1f8e16b02afe84028890f255eec9fbda5b6a72e0
Hummer5235 <92242079+Hummer5235@users.noreply.github.com>
2023-11-18 13:41:59 +0300

    17.11.2023

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ print('Hello World')
 def foo(x:int) -> Tuple [int,float]:
     return x,math.pi
 
+def say_something(number:int, word:str):
+    print(word.)
+
 pprint.pprint('Hello')
 
 print(foo(4))
